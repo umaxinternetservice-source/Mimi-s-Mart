3c74e03517ab13b5c6c45f43dfd219fe5e59f520
umaxinternetservice-source <umaxinternetservice@gmail.com>
2026-02-23 12:28:46 +0600

    Add product route and enable debug mode

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
